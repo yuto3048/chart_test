4545edbcf84ddca0b2800b9d9869a536de900e77
Yuto Fujimura <yuto101024@gmail.com>
2019-01-23 01:06:34 +0900

    Initial commit new file:   .babelrc new file:   .editorconfig new file:   .gitignore new file:   .postcssrc.js new file:   README.md new file:   build/build.js new file:   build/check-versions.js new file:   build/logo.png new file:   build/utils.js new file:   build/vue-loader.conf.js new file:   build/webpack.base.conf.js new file:   build/webpack.dev.conf.js new file:   build/webpack.prod.conf.js new file:   config/dev.env.js new file:   config/index.js new file:   config/prod.env.js new file:   index.html new file:   package-lock.json new file:   package.json new file:   src/App.vue new file:   src/assets/logo.png new file:   src/components/HelloWorld.vue new file:   src/main.js new file:   static/.gitkeep

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <canvas id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import Chart from 'chart.js'
+
+Vue.use(Vuetify)
+
+export default {
+  name: 'App',
+  mounted() {
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
